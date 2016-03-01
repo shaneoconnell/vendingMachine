1854bdf79649543d5262fe6d850bca69fdbe34b4
Shane O Connell <oconnes8@gmail.com>
2016-03-01 16:06:24 +0000

    comment change to check out PyCharm

--- vending_machine.py
+++ vending_machine.py
@@ -1,6 +1,7 @@
 class TestVendingMachine(unittest.TestCase):
     denominations = [1, 2, 5, 10, 20, 50, 100, 200]
     denominations.sort(reverse=True)
+    # hello
     def give_change(self, amount, change):
         if amount == 0:
             return change
